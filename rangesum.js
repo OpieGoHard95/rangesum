9fc5312a0de8a6e932aa6fc8c16736f290f2e542
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-04 11:35:58 -0500

    Fixed the first step in the problem :)

--- rangesum.js
+++ rangesum.js
@@ -3,9 +3,10 @@
 // Code written on 2/3/15
 
 var range = function(start, end) {
-  var rangeArray = [];
-  for (var numRange = start; numRange <= end; numRange + 1);
-    rangeArray.push(numRange); 
+var rangeArray = [];
+  for (var numRange = start; numRange <= end; numRange = numRange + 1)
+	if (numRange <= end)
+   	 rangeArray.push(numRange); 
   return rangeArray;
 }
 
